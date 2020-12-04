7879480268b2184992916ea1ea9fc2e243d9883f
KILIGzzz <email>
2020-12-04 12:03:20 +0800

    test

---
+++ src/git.java
@@ -0,0 +1,12 @@
+/**
+ * @author ：IvanZ
+ * @version : 1.0
+ * @date ：Created in 2020/12/4 12:00
+ * @description ：
+ * @modified By：
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("Xxx");
+    }
+}
